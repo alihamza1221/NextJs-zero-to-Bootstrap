a20bbc15a1f572c7db8564ea69ee79c866fe9f23
alihamza1221 <alihamza77065@gmail.com>
2024-05-22 20:24:07 +0500

    react hook form add

---
+++ tanstack_react_query_form/app/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { useForm } from "react-hook-form";
+
+export default function Home() {
+  const form = useForm();
+  const { register, handleSubmit } = form;
+  const submitHandler = async (data: any): Promise<void> => {
+    console.log(data);
+    console.log(data["username"], "pass:", data["password"]);
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit(submitHandler)}>
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="username" className="sr-only">
+                Username
+              </label>
+              <input
+                id="username"
+                {...register("username")}
+                type="text"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Username"
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+
+              <input
+                id="password"
+                {...register("password")}
+                type="password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Password"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-indigo-500 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
